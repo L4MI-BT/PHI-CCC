98418a20c1b903fd192795364bbd9cb0eb74f3ae
Lucas BT <l.blanchard-toulet@outlook.fr>
2026-02-27 13:00:26 +0100

    Contact

--- src/Views/public/contact.php
+++ src/Views/public/contact.php
@@ -1,1 +1,63 @@
-<h1>Je suis le contact</h1>
+<div class="breadcrum container">
+        <ol class="breadcrumb m-4">
+            <li class="breadcrumb-item"><a href="?controller=global&method=index">Accueil</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Contact</li>
+        </ol>
+    </div>
+    <div class="container">
+        <h1 class="m-5 text-center">CONTACTEZ-NOUS</h1>
+        <div class="row g-5">
+            <div class="col-lg-4 col-sm-12">
+                <div class="box-green shadow rounded p-3">
+                    <h3 class="p-3 text-center">NOTRE AGENCE</h3>
+                    <hr class="p-2">
+                    <div>
+                        <ul>
+                            <li>
+                                <img src="./asset/img/map-pin.svg" alt="" height="15px"> Siège social :
+                                <p>
+                                    534 avenue Jean Giraudoux <br>
+                                    18200 Saint-Amand-Montrond – France <br>
+                                </p>
+                            </li>
+                            <li>
+                                Téléphone : 02 48 60 72 68 <br>
+                                Email : contact-siege@phi.asso.fr <br>
+                                Site : https://www.phi.asso.fr <br>
+                            </li>
+                            <li class="mt-2">
+                                <img src="./Asset/Images/facebook.svg" alt="facebook" height="30px">
+                                <img src="./Asset/Images/insta.svg" alt="instagram" height="30px">
+                                <img src="./Asset/Images/link.svg" alt="linkedn" height="30px">
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-8">
+                <div class="shadow rounded box-green mb-5">
+                    <h2 class="p-3 text-center">ENVOYER NOUS UN MESSAGE</h2>
+                    <hr>
+                    <form action="">
+                        <div class="mb-3 ps-5 pe-5" class="shadow rounded">
+                            <label for="formGroupExampleInput" class="form-label">Nom</label>
+                            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Votre nom...">
+                        </div>
+                        <div class="mb-3 ps-5 pe-5" class="shadow rounded">
+                            <label for="formGroupExampleInput2" class="form-label">Prénom</label>
+                            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Votre prénom...">
+                        </div>
+                        <div class="mb-3 ps-5 pe-5" class="shadow rounded">
+                            <label for="exampleFormControlInput1" class="form-label">Email</label>
+                            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                        </div>
+                        <div class="mb-3 ps-5 pe-5" class="shadow rounded">
+                            <label for="exampleFormControlTextarea1" class="form-label">Votre message</label>
+                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        </div>
+                    <button type="submit" class="btn btn-success-inverse m-4">Envoyer</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
