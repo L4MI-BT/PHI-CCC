94623aff4c42fa91f5b263a6ecaf923da6d6abd4
Lucas BT <l.blanchard-toulet@outlook.fr>
2026-02-27 11:49:04 +0100

    divers

--- src/Views/public/boutique.php
+++ src/Views/public/boutique.php
@@ -1,1 +1,7 @@
-<h1>Je suis le boutique</h1>
+    <div class="breadcrum container">
+        <ol class="breadcrumb m-4">
+            <li class="breadcrumb-item"><a href="?controller=global&method=index">Accueil</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Boutique</li>
+        </ol>
+    </div>
+<h1 class="text-center m-5">NOTRE BOUTIQUE</h1>
